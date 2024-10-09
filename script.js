8bb1243e0e36a1c0b1334d2352b55ca1f51cc409
Maria-Passos <eduardagusmaopassos@gmail.com>
2024-10-08 21:39:46 -0300

    Adicionando evento de clique com JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 alert("Olá do JavaScript!");
 document.addEventListener('DOMContentLoaded', function(){
-	const meuTitulo = document.querySelector('h1');
-meuTitulo.textContent = 'Título modificado pelo JavaScript!';
+	const minhaLista = document.querySelector('ul');
+minhaLista.addEventListener('click',function(){
+	alert('Você clicou na lista!');
+});
 });
